600112ea7c3a3bcb465d4692b210186bd4fc3810
Kikuo Emoto <kemoto@codemonger.io>
2023-09-06 17:14:07 +0900

    feat: experiment partitioning
    - `main` experiments partitioning.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use rand::Rng;
 
 use flechasdb::kmeans::{ Codebook, cluster };
 use flechasdb::linalg::{ dot, norm2, scale_in, subtract_in };
+use flechasdb::partitions::{ Partitioning };
 use flechasdb::vector::{ BlockVectorSet, VectorSet, divide_vector_set };
 
 fn main() -> Result<(), Error> {
     const N: usize = 5000; // number of vectors
     const M: usize = 1024; // vector size
     const D: usize = 8; // number of divisions
-    const C: usize = 25; // number of clusters
+    const P: usize = 10; // number of partitions
+    const C: usize = 25; // number of clusters for PQ
     const MD: usize = M / D; // subvector size
     const K: usize = 10; // K-nearest neighbors
     let use_rkm = false;
@@ -23,10 +25,14 @@ fn main() -> Result<(), Error> {
         let v = vs.get_mut(i);
         scale_in(v, 1.0 / norm2(v));
     }
-    let divided = divide_vector_set(&vs, D.try_into()?)?;
     println!("prepared data in: {} us", time.elapsed().as_micros());
-    // builds codebooks
+    // partitions all the data
     let time = std::time::Instant::now();
+    let partitions = vs.partition(P.try_into().unwrap())?;
+    println!("partitioned data in: {} us", time.elapsed().as_micros());
+    // builds codebooks for residues
+    let time = std::time::Instant::now();
+    let divided = divide_vector_set(&partitions.residues, D.try_into()?)?;
     let mut codebooks: Vec<Codebook<f32>> = Vec::with_capacity(D);
     for subvs in &divided {
         assert_eq!(subvs.vector_size(), MD);
@@ -53,6 +59,7 @@ fn main() -> Result<(), Error> {
             cluster(subvs, C.try_into()?)?
         };
         // intra cluster variance
+        /*
         let mut vector_buf = vec![0.0f32; MD];
         for i in 0..C {
             let mut var = 0.0f32;
@@ -71,81 +78,95 @@ fn main() -> Result<(), Error> {
             }
             var = if count > 1 { var / (count - 1) as f32 } else { var };
             println!("cluster {} variance: {}", i, var);
-        }
+        } */
         codebooks.push(codebook);
     }
     println!("built codebooks in: {} us", time.elapsed().as_micros());
-    // builds a distance table for a query vector
+    // creates a random query vector
+    let mut qv = vec![0.0f32; M];
+    rng.fill(&mut qv[..]);
+    let qv_norm2 = norm2(&qv);
+    scale_in(&mut qv, 1.0 / qv_norm2);
+    // calculates distances from partition centroids
     let time = std::time::Instant::now();
-    let mut v = vec![0.0f32; M];
-    rng.fill(&mut v[..]);
-    let v_norm2 = norm2(&v);
-    scale_in(&mut v, 1.0 / v_norm2);
-    let mut cosine_table: Vec<f32> = Vec::with_capacity(D * C);
-    for i in 0..D {
-        let from = i * MD;
-        let to = from + MD;
-        let subv = &v[from..to];
-        for j in 0..C {
-            let centroid = codebooks[i].centroids.get(j);
-            cosine_table.push(dot(subv, centroid));
-        }
-    }
-    let mut distance_table: Vec<f32> = Vec::with_capacity(D * C);
-    let mut vector_buf = vec![0.0f32; MD];
-    for i in 0..D {
-        let from = i * MD;
-        let to = from + MD;
-        let subv = &v[from..to];
-        for j in 0..C {
-            let d = &mut vector_buf[..];
-            d.copy_from_slice(subv);
-            let centroid = codebooks[i].centroids.get(j);
-            subtract_in(d, centroid);
-            distance_table.push(dot(d, d));
-        }
-    }
-    println!("calculated distance table in: {} us", time.elapsed().as_micros());
-    // calculates distances between the query vector and data vectors
-    let time = std::time::Instant::now();
-    let mut cosines: Vec<(usize, f32)> = Vec::with_capacity(N);
-    let mut distances: Vec<(usize, f32)> = Vec::with_capacity(N);
-    let mut cos_approxes: Vec<(usize, f32)> = Vec::with_capacity(N);
-    let mut dis_approxes: Vec<(usize, f32)> = Vec::with_capacity(N);
+    let mut distances: Vec<f32> = Vec::with_capacity(P);
     let mut vector_buf = vec![0.0f32; M];
-    for i in 0..N {
-        let cosine = 1.0 - dot(&v, vs.get(i));
+    for i in 0..P {
+        let centroid = partitions.codebook.centroids.get(i);
         let d = &mut vector_buf[..];
-        d.copy_from_slice(&v);
-        subtract_in(d, vs.get(i));
-        let distance = dot(d, d);
-        let mut cos_approx = 1.0f32;
-        for j in 0..D {
-            let index = codebooks[j].indices[i];
-            cos_approx -= cosine_table[j * C + index];
-        }
-        let mut dis_approx = 0.0f32;
-        for j in 0..D {
-            let index = codebooks[j].indices[i];
-            dis_approx += distance_table[j * C + index];
-        }
-        cosines.push((i, cosine));
-        distances.push((i, distance));
-        cos_approxes.push((i, cos_approx));
-        dis_approxes.push((i, dis_approx));
+        d.copy_from_slice(&qv[..]);
+        subtract_in(d, centroid);
+        distances.push(norm2(d));
     }
-    distances.sort_by(|x, y| x.1.partial_cmp(&y.1).unwrap());
-    cosines.sort_by(|x, y| x.1.partial_cmp(&y.1).unwrap());
-    cos_approxes.sort_by(|x, y| x.1.partial_cmp(&y.1).unwrap());
-    dis_approxes.sort_by(|x, y| x.1.partial_cmp(&y.1).unwrap());
-    println!("calculated distances in: {} us", time.elapsed().as_micros());
     println!(
-        "{}-nearest neighbors:\ncosine:\n{:?}\napprox:\n{:?}\ndistance:\n{:?}\napprox:\n{:?}",
-        K,
-        &cosines[0..K],
-        &cos_approxes[0..K],
+        "calculated distances from partition centroids in: {} us\n{:?}",
+        time.elapsed().as_micros(),
+        {
+            let mut indexed: Vec<_> = distances.iter().enumerate().collect();
+            indexed.sort_by(|a, b| a.1.partial_cmp(b.1).unwrap());
+            indexed
+        },
+    );
+    // calculates approximate distances through partitioned PQ
+    let time = std::time::Instant::now();
+    let mut approxes: Vec<(usize, f32)> = Vec::with_capacity(N);
+    let mut vector_buf = vec![0.0f32; M];
+    for i in 0..P {
+        let time = std::time::Instant::now();
+        let mut distance_table = Vec::with_capacity(D * C);
+        let centroid = partitions.codebook.centroids.get(i);
+        let localized = &mut vector_buf[..];
+        localized.copy_from_slice(&qv[..]);
+        subtract_in(localized, centroid);
+        let mut vector_buf = vec![0.0f32; MD];
+        for j in 0..D {
+            let from = j * MD;
+            let to = from + MD;
+            let subqv = &localized[from..to];
+            for k in 0..C {
+                let d = &mut vector_buf[..];
+                d.copy_from_slice(subqv);
+                let centroid = codebooks[j].centroids.get(k);
+                subtract_in(d, centroid);
+                distance_table.push(dot(d, d));
+            }
+        }
+        for (j, _) in partitions.codebook.indices
+            .iter()
+            .enumerate()
+            .filter(|(_, &pi)| pi == i)
+        {
+            let mut distance = 0.0f32;
+            for k in 0..D {
+                let centroid_index = codebooks[k].indices[j];
+                distance += distance_table[k * C + centroid_index];
+            }
+            approxes.push((j, distance));
+        }
+        println!(
+            "approximated distances in partition {} in {} us",
+            i,
+            time.elapsed().as_micros(),
+        );
+    }
+    println!("approximated distances in {} us", time.elapsed().as_micros());
+    // calculates the true distances
+    let time = std::time::Instant::now();
+    let mut distances: Vec<(usize, f32)> = Vec::with_capacity(N);
+    for (i, mut v) in partitions.all_vectors().enumerate() {
+        let d = &mut v[..];
+        subtract_in(d, &qv);
+        let distance = dot(d, d);
+        distances.push((i, distance));
+    }
+    println!("calculated distances in {} us", time.elapsed().as_micros());
+    // reports the K nearest neighbors
+    approxes.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    distances.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    println!(
+        "k-NN results:\ndistance:\n{:?}\napproximation:\n{:?}",
         &distances[0..K],
-        &dis_approxes[0..K],
+        &approxes[0..K],
     );
     Ok(())
 }
