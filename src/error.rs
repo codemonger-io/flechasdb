5da73725ceca9a20a683f18165083c61edbf5afd
Kikuo Emoto <kemoto@codemonger.io>
2023-09-09 09:11:35 +0900

    feat(error): coerce misc errors
    - `error` module coerces miscellaneous errors:
        - I/O Error
        - error from `protobuf`
        - error from `templfile`

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,10 @@
 pub enum Error {
     /// Invalid arguments.
     InvalidArgs(String),
+    /// I/O error.
+    IOError(std::io::Error),
+    /// Error on `protobuf`.
+    ProtobufError(protobuf::Error),
 }
 
 impl Error {
@@ -10,6 +14,8 @@ impl Error {
     pub fn to_string(self) -> String {
         match self {
             Self::InvalidArgs(s) => s,
+            Self::IOError(e) => format!("I/O error: {}", e),
+            Self::ProtobufError(e) => format!("Protobuf error: {}", e),
         }
     }
 }
@@ -19,5 +25,23 @@ impl std::error::Error for Error {}
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.to_string())
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Self::IOError(e)
+    }
+}
+
+impl From<protobuf::Error> for Error {
+    fn from(e: protobuf::Error) -> Self {
+        Self::ProtobufError(e)
+    }
+}
+
+impl From<tempfile::PersistError> for Error {
+    fn from(e: tempfile::PersistError) -> Self {
+        Self::IOError(e.error)
     }
 }
