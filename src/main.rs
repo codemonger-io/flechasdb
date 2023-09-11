c832a3be2c54bbe24de53b9e8c36d030c1310f96
Kikuo Emoto <kemoto@codemonger.io>
2023-09-11 15:52:43 +0900

    feat: experiment loading database

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,26 @@ use flechasdb::db::{
     DatabaseQueryEvent,
 };
 use flechasdb::db::proto::serialize_database;
+use flechasdb::db::stored;
+use flechasdb::db::stored::{DatabaseStore, LoadDatabase};
 use flechasdb::io::LocalFileSystem;
 use flechasdb::linalg::{ norm2, scale_in };
 use flechasdb::vector::{ BlockVectorSet, VectorSet };
 
 fn main() -> Result<(), Error> {
+    let args: Vec<String> = std::env::args().collect();
+    match args.get(1) {
+        None => generate(),
+        Some(s) if s == "generate" => generate(),
+        Some(s) if s == "load" => load(&args[2]),
+        _ => {
+            println!("usage: {} [generate|query]", args[0]);
+            Ok(())
+        },
+    }
+}
+
+fn generate() -> Result<(), Error> {
     const N: usize = 5000; // number of vectors
     const M: usize = 1024; // vector size
     const D: usize = 8; // number of divisions
@@ -75,10 +90,7 @@ fn main() -> Result<(), Error> {
         }))?;
     println!("built database in {} μs", time.elapsed().as_micros());
     // creates a random query vector
-    let mut qv = vec![0.0f32; M];
-    rng.fill(&mut qv[..]);
-    let qv_norm2 = norm2(&qv);
-    scale_in(&mut qv, 1.0 / qv_norm2);
+    let qv = random_query_vector(&mut rng, M);
     // queries k-NN
     let time = std::time::Instant::now();
     let mut event_time = std::time::Instant::now();
@@ -126,6 +138,63 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
+fn load<P>(path: P) -> Result<(), Error>
+where
+    P: AsRef<std::path::Path> + core::fmt::Debug,
+{
+    const K: usize = 10; // K-nearest neighbors
+    const NP: usize = 3; // number of partitions to query
+    // loads the database
+    println!("loading database from {:?}", path);
+    let time = std::time::Instant::now();
+    let db = load_database(path)?;
+    println!("loaded database in {} μs", time.elapsed().as_micros());
+    // randomly generates a query vector.
+    let mut rng = rand::thread_rng();
+    let qv = random_query_vector(&mut rng, db.vector_size());
+    // queries k-NN
+    let time = std::time::Instant::now();
+    let mut event_time = std::time::Instant::now();
+    let results = db.query(
+        qv,
+        K.try_into().unwrap(),
+        NP.try_into().unwrap(),
+        Some(move |event| {
+            match event {
+                stored::DatabaseQueryEvent::StartingPartitionSelection |
+                stored::DatabaseQueryEvent::StartingPartitionQuery(_) |
+                stored::DatabaseQueryEvent::StartingResultSelection => {
+                    event_time = std::time::Instant::now();
+                },
+                stored::DatabaseQueryEvent::FinishedPartitionSelection => {
+                    println!(
+                        "selected partitions in {} μs",
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+                stored::DatabaseQueryEvent::FinishedPartitionQuery(i) => {
+                    println!(
+                        "queried partition {} in {} μs",
+                        i,
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+                stored::DatabaseQueryEvent::FinishedResultSelection => {
+                    println!(
+                        "selected results in {} μs",
+                        event_time.elapsed().as_micros(),
+                    );
+                },
+            }
+        })
+    )?;
+    println!("queried k-NN in {} μs", time.elapsed().as_micros());
+    for (i, result) in results.iter().enumerate() {
+        println!("{}: {:?}", i, result);
+    }
+    Ok(())
+}
+
 fn save_database<VS, P>(
     db: &Database<f32, VS>,
     base_path: P,
@@ -138,4 +207,32 @@ where
     let mut fs = LocalFileSystem::new(base_path);
     serialize_database(db, &mut fs)?;
     Ok(())
+}
+
+fn load_database<P>(
+    path: P,
+) -> Result<stored::Database<f32, LocalFileSystem>, Error>
+where
+    P: AsRef<std::path::Path> + core::fmt::Debug,
+{
+    let db = DatabaseStore::<f32, _>::load_database(
+        LocalFileSystem::new(path.as_ref().parent().unwrap()),
+        path.as_ref().file_name().unwrap(),
+    )?;
+    Ok(db)
+}
+
+fn random_query_vector<R>(rng: &mut R, size: usize) -> Vec<f32>
+where
+    R: Rng,
+{
+    let mut v: Vec<f32> = Vec::with_capacity(size);
+    unsafe {
+        let p = v.as_mut_ptr();
+        rng.fill(core::slice::from_raw_parts_mut(p, size));
+        v.set_len(size);
+    }
+    let norm = norm2(&v);
+    scale_in(&mut v, 1.0 / norm);
+    v
 }
