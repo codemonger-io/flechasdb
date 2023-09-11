8b4a09d2380d2706807c36f1f553dfc1c19d22c4
Kikuo Emoto <kemoto@codemonger.io>
2023-09-11 15:31:35 +0900

    feat(io): add input hashed file
    - `io` introduces a new trait `HashedFileIn` that represents a file
      whose name is the SHA-256 digest of its contents. `FileSystem`
      introduces a new function `open_hashed_file` that opens such a file.
    
      Renames `HashedFile` → `HashedFileOut`.
    
      `FileSystem` becomes immutable.

--- src/io.rs
+++ src/io.rs
@@ -5,7 +5,7 @@ use base64::{
     engine::general_purpose::{URL_SAFE_NO_PAD as base64_engine},
 };
 use std::ffi::OsStr;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use tempfile::NamedTempFile;
 
@@ -14,32 +14,53 @@ use crate::error::Error;
 /// Abstracts a file system.
 pub trait FileSystem {
     /// File whose name will be the hash of its contents.
-    type HashedFile: HashedFile;
+    type HashedFileOut: HashedFileOut;
+    /// File whose name is the hash of its contents.
+    type HashedFileIn: HashedFileIn;
 
     /// Creates a file whose name will be the hash of its contents.
-    fn create_hashed_file(&mut self) -> Result<Self::HashedFile, Error>;
+    fn create_hashed_file(&self) -> Result<Self::HashedFileOut, Error>;
 
     /// Creates a hashed file in a given directory.
     fn create_hashed_file_in<P>(
-        &mut self,
+        &self,
         path: P,
-    ) -> Result<Self::HashedFile, Error>
+    ) -> Result<Self::HashedFileOut, Error>
+    where
+        P: AsRef<Path>;
+
+    /// Opens a file whose name is the hash of its contents.
+    fn open_hashed_file<P>(
+        &self,
+        path: P,
+    ) -> Result<Self::HashedFileIn, Error>
     where
         P: AsRef<Path>;
 }
 
 /// File whose name will be the hash of its contents.
-pub trait HashedFile: Write {
+pub trait HashedFileOut: Write {
     /// Persists the file.
     ///
     /// Finishes the calculation of the hash and persists the file.
     /// You should flush the stream before calling this function.
     ///
-    /// Returns the encoded hash value that is supposed to be Base64 encoded
-    /// SHA256 digest.
+    /// Returns the encoded hash value that is supposed to be a URS-safe Base64
+    /// encoded SHA256 digest.
     fn persist<S>(self, extension: S) -> Result<String, Error>
     where
         S: AsRef<OsStr>;
+}
+
+/// File whose name is the hash of its contents.
+pub trait HashedFileIn: Read {
+    /// Verifies the file.
+    ///
+    /// Finishes the calculation of the hash and verifies the file.
+    /// You should call this function after the entire file has been read.
+    ///
+    /// File name is supposed to be a Base64 encoded URL-safe SHA256 digest.
+    fn verify(self) -> Result<(), Error>;
 }
 
 /// File system uses the local file system.
@@ -61,40 +82,51 @@ impl LocalFileSystem {
 }
 
 impl FileSystem for LocalFileSystem {
-    type HashedFile = LocalHashedFile;
+    type HashedFileOut = LocalHashedFileOut;
+    type HashedFileIn = LocalHashedFileIn;
 
-    fn create_hashed_file(&mut self) -> Result<Self::HashedFile, Error> {
-        LocalHashedFile::create(self.base_path.clone())
+    fn create_hashed_file(&self) -> Result<Self::HashedFileOut, Error> {
+        LocalHashedFileOut::create(self.base_path.clone())
     }
 
     fn create_hashed_file_in<P>(
-        &mut self,
+        &self,
         path: P,
-    ) -> Result<Self::HashedFile, Error>
+    ) -> Result<Self::HashedFileOut, Error>
     where
         P: AsRef<Path>,
     {
-        LocalHashedFile::create(self.base_path.join(path))
+        LocalHashedFileOut::create(self.base_path.join(path))
+    }
+
+    fn open_hashed_file<P>(
+        &self,
+        path: P,
+    ) -> Result<Self::HashedFileIn, Error>
+    where
+        P: AsRef<Path>,
+    {
+        LocalHashedFileIn::open(self.base_path.join(path))
     }
 }
 
-/// File in the local file system.
+/// Writable file in the local file system.
 ///
 /// Created as a temporary file and renamed to the hash of its contents.
-pub struct LocalHashedFile {
+pub struct LocalHashedFileOut {
     // Temporary file.
     tempfile: NamedTempFile,
     // Persisted path.
     base_path: PathBuf,
-    // Context to calculate SHA-256 hash.
+    // Context to calculate an SHA-256 digest.
     context: ring::digest::Context,
 }
 
-impl LocalHashedFile {
+impl LocalHashedFileOut {
     /// Creates a temporary file to be persisted under a given path.
     fn create(base_path: PathBuf) -> Result<Self, Error> {
         let tempfile = NamedTempFile::new()?;
-        Ok(LocalHashedFile {
+        Ok(LocalHashedFileOut {
             tempfile,
             base_path,
             context: ring::digest::Context::new(&ring::digest::SHA256),
@@ -102,7 +134,7 @@ impl LocalHashedFile {
     }
 }
 
-impl Write for LocalHashedFile {
+impl Write for LocalHashedFileOut {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         self.context.update(buf);
         self.tempfile.write(buf)
@@ -113,7 +145,7 @@ impl Write for LocalHashedFile {
     }
 }
 
-impl HashedFile for LocalHashedFile {
+impl HashedFileOut for LocalHashedFileOut {
     fn persist<S>(mut self, extension: S) -> Result<String, Error>
     where
         S: AsRef<OsStr>,
@@ -127,5 +159,49 @@ impl HashedFile for LocalHashedFile {
         let path = self.base_path.join(&hash).with_extension(extension);
         self.tempfile.persist(path)?;
         Ok(hash)
+    }
+}
+
+/// Readable file in the local file system.
+pub struct LocalHashedFileIn {
+    file: std::fs::File,
+    path: PathBuf,
+    // Context to calculate an SHA-256 digest.
+    context: ring::digest::Context,
+}
+
+impl LocalHashedFileIn {
+    /// Opens a file whose name is the hash of its contents.
+    fn open(path: PathBuf) -> Result<Self, Error> {
+        let file = std::fs::File::open(&path)?;
+        Ok(LocalHashedFileIn {
+            file,
+            path,
+            context: ring::digest::Context::new(&ring::digest::SHA256),
+        })
+    }
+}
+
+impl Read for LocalHashedFileIn {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let n = self.file.read(buf)?;
+        self.context.update(&buf[..n]);
+        Ok(n)
+    }
+}
+
+impl HashedFileIn for LocalHashedFileIn {
+    fn verify(self) -> Result<(), Error> {
+        let hash = self.context.finish();
+        let hash = base64_engine.encode(&hash);
+        if hash.as_str() == self.path.file_stem().unwrap_or(OsStr::new("")) {
+            Ok(())
+        } else {
+            Err(Error::VerificationFailure(format!(
+                "Expected hash {:?}, but got {}",
+                self.path.file_stem(),
+                hash,
+            )))
+        }
     }
 }
