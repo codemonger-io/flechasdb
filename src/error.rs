4acde0aafb946229ae89938c82280ee80850f1f3
Kikuo Emoto <kemoto@codemonger.io>
2023-09-18 06:51:00 +0900

    fix(error): stack overflow at anyhow coercion
    - Fixes the issue that stack overflow happened when `Error` was coerced
      to `anyhow::Error`. Custom `to_string` implementation caused infinite
      recursions. Copies `to_string` implementation to `Display::fmt` and
      removes `to_string`.

--- src/error.rs
+++ src/error.rs
@@ -13,24 +13,17 @@ pub enum Error {
     ProtobufError(protobuf::Error),
 }
 
-impl Error {
-    /// Converts into a string.
-    pub fn to_string(self) -> String {
-        match self {
-            Self::InvalidArgs(s) |
-            Self::InvalidData(s) |
-            Self::VerificationFailure(s) => s,
-            Self::IOError(e) => format!("I/O error: {}", e),
-            Self::ProtobufError(e) => format!("Protobuf error: {}", e),
-        }
-    }
-}
-
 impl std::error::Error for Error {}
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_string())
+        match self {
+            Self::InvalidArgs(s) |
+            Self::InvalidData(s) |
+            Self::VerificationFailure(s) => write!(f, "{}", s),
+            Self::IOError(e) => write!(f, "I/O error: {}", e),
+            Self::ProtobufError(e) => write!(f, "Protobuf error: {}", e),
+        }
     }
 }
 
