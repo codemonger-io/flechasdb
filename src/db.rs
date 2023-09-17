14ef275f41496ca5587723c6d803731e1fc3ce11
Kikuo Emoto <kemoto@codemonger.io>
2023-09-18 08:51:13 +0900

    style(db): remove extra space

--- src/db.rs
+++ src/db.rs
@@ -11,7 +11,7 @@ use uuid::Uuid;
 use crate::error::Error;
 use crate::kmeans::{Codebook, Scalar, cluster};
 use crate::linalg::{dot, subtract_in};
-use crate::partitions::{ Partitioning, Partitions };
+use crate::partitions::{Partitioning, Partitions};
 use crate::slice::AsSlice;
 use crate::vector::{BlockVectorSet, VectorSet, divide_vector_set};
 
